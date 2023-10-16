8e073ba2596d929848aaa6c163f6bb03639a33c0
xiaoyang <xiao.yang3@northeastern.edu>
2023-10-15 22:47:51 -0700

    implement FileSelector Component

---
+++ src/Components/FileSelector.tsx
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react';
+import "./FileSelector.css";
+
+
+  
+interface Props {
+    fetchFiles: () => Promise<string[]>;
+    onFileSelect: (filename: string, userName: string) => void;
+    userName: string;
+}
+const FileSelector: React.FC<Props> = ({fetchFiles, onFileSelect, userName}) => {
+
+    const [files, setFiles] = useState<string[]>([]);
+
+    useEffect(() => {
+        fetchFiles().then(setFiles);
+        //setFiles(f);
+    }, [fetchFiles]);
+  
+    function handleChange(e: React.ChangeEvent<HTMLSelectElement>) {
+        onFileSelect(e.target.value, userName);
+    }
+
+    return (
+        <div>
+            <label >SELECT A SHEET</label>
+            <select onChange={handleChange}>
+            {files.map(f => 
+                <option key={f} value={f}>{f}</option>
+            )}
+            </select>
+        </div>
+    );
+}
+export default FileSelector;
