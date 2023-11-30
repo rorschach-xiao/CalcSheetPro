07b4d9b025189d19642d7966896bc1913c1d4128
xiaoyang <xiao.yang3@northeastern.edu>
2023-11-30 00:27:50 -0800

    config whiteboard server url[DEPLOY]

--- src/ServerDataDefinitions.ts
+++ src/ServerDataDefinitions.ts
@@ -14,4 +14,4 @@ export const LOCAL_REDIS_URL = "http://localhost";
 export const RENDER_SERVER_URL = "https://calcsheetpro.onrender.com";
 export const RENDER_CHAT_SERVER_URL = "https://calcsheetpro-chat.onrender.com";
 export const RENDER_REDIS_URL = "redis://red-cl42doquuipc738pbibg";
-export const RENDER_WHITEBOARD_SERVER_URL = "https://calcsheetpro-client.onrender.com";
+export const RENDER_WHITEBOARD_SERVER_URL = "https://calsheetpro-whiteboard.onrender.com";
